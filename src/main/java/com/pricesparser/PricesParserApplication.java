4f29f0128086a1a589a677299822a0b50cb3bc48
ermirgurt <erik1297e@gmail.com>
2025-11-21 02:56:27 +0300

    feat: init project

---
+++ src/main/java/com/pricesparser/PricesParserApplication.java
@@ -0,0 +1,13 @@
+package com.pricesparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricesParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PricesParserApplication.class, args);
+    }
+}
+
